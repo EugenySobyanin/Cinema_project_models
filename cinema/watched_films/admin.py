3019418cae7d715d1f40c1858682e352f66e733c
Евгений Собянин <ea.sobyanin2023@gmail.com>
2024-04-20 16:56:48 +0300

    Добавил requirements.txt, зарегал модели в админке

--- cinema/watched_films/admin.py
+++ cinema/watched_films/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WatchedFilms, OriginalTitle, Genre, ProductType
 
+
+admin.site.register(WatchedFilms)
+admin.site.register(OriginalTitle)
+admin.site.register(Genre)
+admin.site.register(ProductType)
 # Register your models here.
