59a5a81ea190044a52f5049fb8130ac2c3e978a9
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-04-23 17:41:35 +0300

    Изменены модели, добавлены фикстуры

--- cinema/watched_films/models.py
+++ cinema/watched_films/models.py
@@ -1,24 +1,55 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+from datetime import datetime
+
+cur_year = datetime.now().year
 
 # модель с оригинальными названиями фильмов  
 class OriginalTitle(models.Model):
     title = models.CharField(max_length=128, unique=True)
+    add_time = models.DateTimeField(auto_created=True, null=True)
     
-    
+
+    class Meta:
+        ordering = ('add_time',)
+        
+            
 # модель c жанрами фильмов
 class Genre(models.Model):
     genre_title = models.CharField(max_length=128, unique=True)
+    add_time = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ('add_time',)
+        
     
-    
+    def __str__(self):
+        return self.genre_title   
+
+
 # модель с типами (например: фильм, мультфильм, сериал)
 class ProductType(models.Model):
     type_title = models.CharField(max_length=128)
+    
+    
+    class Meta:
+        ordering = ('id',)
+        
+        
+    def __str__(self):
+        return self.type_title
     
 
 # моделль с просмотренными фильмами
 class WatchedFilms(models.Model):
     title = models.CharField(max_length=128, unique=True)
+    year = models.PositiveSmallIntegerField(null=True, validators=
+                                            [MinValueValidator(1900), 
+                                             MaxValueValidator(cur_year + 20)
+                                             ]
+                                            )
     original_title = models.OneToOneField(
         OriginalTitle,
         blank=True, 
@@ -33,6 +64,16 @@ class WatchedFilms(models.Model):
     )
     genres = models.ManyToManyField(Genre, blank=False)
     country = models.CharField(max_length=128)
+    add_time = models.DateTimeField(auto_now_add=True, null=True)
+
+    
+    
+    class Meta:
+        ordering = ('add_time',)
+        
+        
+    def __str__(self):
+        return self.title
     
 
     
