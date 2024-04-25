830fe7267a51e1146e6f72eef4a3884f64cc024b
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-04-25 18:00:39 +0300

    Работа над стилями и шаблонами

--- cinema/homepage/views.py
+++ cinema/homepage/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return render(request, "base.html")
+    return render(request, "homepage/homepage.html")
     
